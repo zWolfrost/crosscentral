462d0dad5819edc5495f03c5083695445465fab8
Luca Russo <zwolfrost@gmail.com>
2026-01-16 21:15:11 +0100

    log

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import mimetypes
 
 BASE_DIR = os.path.abspath("/srv/share")
 
+mimetypes.add_type("text-plain", ".log")
 mimetypes.add_type("audio-x-flac", ".flac")
 
 app = Flask(__name__, static_folder="static", template_folder="static")
